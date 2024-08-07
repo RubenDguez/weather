41195d16b6045d718185116b858f8631409f2dd5
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-08-06 21:59:13 -0400

    update server static files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,22 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path, { join } from 'path';
+
 dotenv.config({
     path: join(process.cwd(), '../.env')
 });
 
 // Import the routes
 import routes from './routes/index.js';
-import { join } from 'path';
+
+const dist = path.join(process.cwd(), '../client/dist')
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-app.use(express.static('client/dist'));
+app.use(express.static(dist));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
